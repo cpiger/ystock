b8b9fd992a07d4826e392a45f11ed466bbf03cdc
Sero Hsueh <sero.hsueh@tw.promise.com>
2016-12-13 17:55:35 +0800

    use ES7 fetch to get page data

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,32 @@
 import './sass/main.scss';
+import 'babel-polyfill';
 
-var h1 = document.createElement('h1');
-h1.innerHTML = 'Hello world!!!';
-h1.className = 'test';
-document.body.appendChild(h1);
+var dd = document.createElement('div');
+dd.innerHTML = 'aaaa';
+dd.className = 'test';
+document.body.appendChild(dd);
+
+// let response = await fetch(this.comicUrl);
+// let rtxt = await response.text();
+
+(async() => {
+  try {
+    var url = "https://tw.stock.yahoo.com/q/bc?s=5478";
+    var response = await fetch(url, {
+      headers: {  
+        "Content-type": "text/html; charset=big5"  
+      }      
+    });
+    var rps = await response.text();
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(rps,"text/html");
+    var q = doc.querySelector('title').text;
+    var google = document.createElement('div');
+    google.innerHTML = q;
+    // console.log(rps);
+    document.body.appendChild(google);
+  } catch (e) {
+    console.log("Booo");
+    console.log(e);
+  }
+})();
