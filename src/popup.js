333548460c213d128d7e1fb2d67c86a890f7b74c
Sero Hsueh <sero.hsueh@tw.promise.com>
2016-12-13 18:57:51 +0800

    try to fix charset issue

--- src/popup.js
+++ src/popup.js
@@ -9,21 +9,28 @@ document.body.appendChild(dd);
 // let response = await fetch(this.comicUrl);
 // let rtxt = await response.text();
 
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
 (async() => {
   try {
     var url = "https://tw.stock.yahoo.com/q/bc?s=5478";
-    var response = await fetch(url, {
-      headers: {  
-        "Content-type": "text/html; charset=big5"  
-      }      
-    });
-    var rps = await response.text();
+    // var response = await fetch(url);
+    // var rps = await response.text();
+    var rps = httpGet(url);
     let parser = new DOMParser();
     let doc = parser.parseFromString(rps,"text/html");
     var q = doc.querySelector('title').text;
     var google = document.createElement('div');
-    google.innerHTML = q;
+    google.innerText = q;
+    // google.textContent = ""
     // console.log(rps);
+    console.log(google.innerHTML);
     document.body.appendChild(google);
   } catch (e) {
     console.log("Booo");
