6923765a6215c61eb18f480472d30e3d6d2ccc9c
CelloCello <cello1124@gmail.com>
2018-05-23 17:42:04 +0800

    adjust tab style to put on bottom

--- src/components/StockTable/TabContainer.js
+++ src/components/StockTable/TabContainer.js
@@ -7,7 +7,7 @@ class TabContainer extends React.Component {
     super(props);
 
     this.tabs = [
-      {},{},{}
+      {stocks: []},{stocks: []},{stocks: this.props.stocks},{stocks: []},{stocks: []}
     ];
   }
 
@@ -25,14 +25,14 @@ class TabContainer extends React.Component {
           key={index}
           tabId={`tab_table_${index}`}
           isActive={index === 0}
-          stocks={this.props.stocks}
+          stocks={item.stocks}
         />
       );
 
       menu.push(
         <li className={index === 0 ? "active" : ""} key={index}>
           <a data-toggle="tab" href={`#tab_table_${index}`}>
-            <span className="glyphicon glyphicon-tag" aria-hidden="true"></span>{index}
+            {index + 1}
           </a>
         </li>
       );
@@ -48,13 +48,22 @@ class TabContainer extends React.Component {
 
     return (
       <div className="tab-table">
-        <ul className="nav nav-tabs">
+        {/* <ul className="nav nav-pills nav-stacked col-xs-1">
           {menu}
         </ul>
-      
+
+        <div className="tab-content col-xs-11">
+          {tabs}
+        </div> */}
+
         <div className="tab-content">
           {tabs}
         </div>
+
+        <ul className="nav nav-tabs">
+          {menu}
+        </ul>
+
       </div>
     )
   }
