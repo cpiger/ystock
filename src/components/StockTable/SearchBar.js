bbddc819b7e8ef6c9bf9a1d97328113b0299f1b4
CelloCello <cello1124@gmail.com>
2018-06-01 10:53:56 +0800

    use chinese

--- src/components/StockTable/SearchBar.js
+++ src/components/StockTable/SearchBar.js
@@ -31,7 +31,7 @@ class SearchBar extends React.Component {
     return (
       <div className="input-group">
         <input type="text" className="form-control" value={this.state.query} 
-          onChange={this.handleChange} onKeyPress={this.handleEnter} placeholder="Search for..." />
+          onChange={this.handleChange} onKeyPress={this.handleEnter} placeholder="輸入台股代號/名稱" />
         <span className="input-group-btn">
           <button className="btn btn-default" type="button" onClick={this.onBtnSearch}>
             <span className="glyphicon glyphicon-search" aria-hidden="true"></span>
